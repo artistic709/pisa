879bcce3a5c7ce53914aa51d35232f9447e6b2c6
Anton <ched6507@gmail.com>
2017-10-26 15:04:03 +0800

    ad_6 version

--- Adseller_2.sol
+++ Adseller_2.sol
@@ -1,26 +1,25 @@
-pragma solidity ^0.4.10; 
+pragma solidity ^0.4.10; //We have to specify what version of the compiler this code will use
 
 contract Adseller {
 
+  // We use the struct datatype to store the voter information.
   struct player {
     address playerAddress; 
     uint tokensBought;    
     uint balanceTokens;
-    uint[] involvedAds;
-    mapping (uint => uint) involvedAdsBids;
+
+    bytes32[] involvedAds;
+    mapping (bytes32 => uint) involvedAdsBids;
   }
 
     struct ad{
-        uint adID;
         bytes32 adname;
         bytes32 content;
-        bytes32 host_url;
-        bytes32 img_url;
     }
 
   mapping (address => player) public playerInfo;
-  mapping (uint => uint) public highestBid;
-  mapping (uint => address) public highestBidder;
+  mapping (bytes32 => uint) public highestBid;
+  mapping (bytes32 => address) public highestBidder;
 
   ad[] public AdList;
 
@@ -35,69 +34,60 @@ contract Adseller {
     tokenPrice = pricePerToken;
   }
   
-  function addAd(bytes32 adname, bytes32 host_url, 
-  bytes32 content, bytes32 img_url) {
-      uint adID = AdList.length + 1;
+  function addAd(bytes32 adname, bytes32 content){
+      for(uint i = 0; i < AdList.length; i++) {
+          if (AdList[i].adname == adname) revert();
+      }
       count_ad ++ ;
-      AdList.push(ad({adname:adname, adID:adID,host_url: host_url, content:content, img_url:img_url}));
+      AdList.push(ad({adname: adname, content:content}));
   }
 
 
-  function updateAd(uint adID, bytes32 content, bytes32 img_url, bytes32 host_url){
-    if (highestBidder[adID] == msg.sender){
-      AdList[adID].content = content;
-      AdList[adID].img_url = img_url;
-      AdList[adID].host_url = host_url;
-    }
-    else {revert();}
-  }
-
-
-  function bidForAd(uint adID, uint bidInTokens) {
-    uint index = adID;
+  function bidForAd(bytes32 adID, uint bidInTokens) {
+    uint index = indexOfAd(adID);
     player thisPlayer = playerInfo[msg.sender];
+    if (index == uint(-1)) revert();
 
-    // Make sure this player has enough tokens to cast
+    // Make sure this player has enough tokens to cast the vote
     
     uint availableTokens = thisPlayer.tokensBought - totalTokensUsed(msg.sender);
     if (availableTokens < bidInTokens) revert();
 
     if (highestBid[adID] < bidInTokens){
- 
+        // able to bid
         
         bool first_time = true;
         for (uint i=0; i < thisPlayer.involvedAds.length; i++ ){
-            if (thisPlayer.involvedAds[i] == adID) 
-                {
-                    first_time = false;
-                    break;
-                }
+            if (thisPlayer.involvedAds[i] == adID) first_time=false;
         }
         if (first_time) thisPlayer.involvedAds.push(adID);
-        
-        
-        // payback the previous bidder 
-        player previous_bidder =  playerInfo[highestBidder[adID]];
-        uint previous_bid = highestBid[adID];
-        previous_bidder.balanceTokens += previous_bid;
 
-      
       highestBid[adID] = bidInTokens;
       highestBidder[adID] = thisPlayer.playerAddress;
       thisPlayer.involvedAdsBids[adID] += bidInTokens;
     }
   }
 
-  
+  // Return the sum of all the tokens used by this player.
   function totalTokensUsed(address user) private constant returns (uint) {
-    uint[] _involvedAds = playerInfo[user].involvedAds;
+    bytes32[] _involvedAds = playerInfo[user].involvedAds;
     uint totalUsedTokens = 0;
     for(uint i = 0; i < _involvedAds.length; i++) {
       totalUsedTokens += playerInfo[user].involvedAdsBids[_involvedAds[i]];
     }
     return totalUsedTokens;
   }
+  
 
+  function indexOfAd(bytes32 adID) constant returns (uint) {
+    for(uint i = 0; i < AdList.length; i++) {
+      if (AdList[i].adname == adID) {
+        return i;
+      }
+    }
+    return uint(-1);
+  }
+  
   
   function buy() payable returns (uint) {
     uint tokensToBuy = msg.value / tokenPrice;
@@ -114,7 +104,7 @@ contract Adseller {
     return totalTokens - balanceTokens;
   }
 
-  function voterDetails(address userAdr) constant returns (uint, uint[]) {
+  function voterDetails(address userAdr) constant returns (uint, bytes32[]) {
     return (playerInfo[userAdr].tokensBought, playerInfo[userAdr].involvedAds);
   }
 
@@ -130,11 +120,11 @@ contract Adseller {
     return Adnamelist;
   }
 
-  function highestBidFor(uint adID) constant returns (uint) {
+  function highestBidFor(bytes32 adID) constant returns (uint) {
     return highestBid[adID];
   }
 
-  function highestBidderFor(uint adID) constant returns (address) {
+  function highestBidderFor(bytes32 adID) constant returns (address) {
     return highestBidder[adID];
   }
 }
